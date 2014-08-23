5d8037f3a0e4d13a566e4cf61723000f5a06a79e
Claudio Ortolina <claudio@new-bamboo.co.uk>
2014-08-23 18:52:01 +0100

    Not sure how this typo made it in.

--- lib/models/delta_over_time.ex
+++ lib/models/delta_over_time.ex
@@ -9,7 +9,7 @@ defmodule DeltaOverTime do
   def from_query_result(query_result) do
     columns = query_result.columns
     Enum.map(query_result.rows, fn(row) ->
-      Enum.zip(columns, row |> Tupl.to_list)
+      Enum.zip(columns, row |> Tuple.to_list)
       |> Enum.reduce(%DeltaOverTime{}, fn({col_name, value}, acc) ->
         Map.put(acc, col_name |> String.to_atom, value)
       end)
