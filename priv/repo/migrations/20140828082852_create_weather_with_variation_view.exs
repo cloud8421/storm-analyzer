ec99b527d36222cf4da3197ab09d6f6301a2dcf9
Claudio Ortolina <claudio@new-bamboo.co.uk>
2014-08-28 09:51:17 +0100

    Small refactor on view creation.

--- priv/repo/migrations/20140828082852_create_weather_with_variation_view.exs
+++ priv/repo/migrations/20140828082852_create_weather_with_variation_view.exs
@@ -4,14 +4,9 @@ defmodule Repo.Migrations.CreateWeatherWithVariationView do
   def up do
     """
     CREATE OR REPLACE VIEW weather_datapoints_with_variation AS
-      SELECT uuid,
-        temperature,
+      SELECT weather_datapoints.*,
         lag(temperature, 1) OVER (order by timestamp) as previous_temperature,
-        external_temperature,
-        lag(external_temperature, 1) OVER (order by timestamp) as previous_external_temperature,
-        brightness,
-        summary,
-        timestamp
+        lag(external_temperature, 1) OVER (order by timestamp) as previous_external_temperature
       FROM weather_datapoints;
     """
   end
